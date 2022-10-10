acf215cbcf0221895afdc599b564ce9573dec615
johan <damstromjohan@gmail.com>
2022-10-10 15:27:56 +0200

    added test

--- src/test/java/InventoryTest.java
+++ src/test/java/InventoryTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class InventoryTest {
 
@@ -12,4 +13,13 @@ public class InventoryTest {
         inventory.add(product, 1);
         assertEquals(1, inventory.getAmount(product));
     }
+
+    @Test
+    void EANIsTooShort() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            EAN ean = new EAN(123456789);
+        });
+    }
+
+
 }
