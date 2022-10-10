16a89e9a6220c91cd8c0b0d4fb892af8ba3a4de6
johan <damstromjohan@gmail.com>
2022-10-10 14:07:24 +0200

    Here comes the EAN class

---
+++ src/main/java/EAN.java
@@ -0,0 +1,11 @@
+public class EAN {
+    private final long eanCode;
+
+    public EAN(long eanCode) {
+        this.eanCode = eanCode;
+    }
+
+    public long getEanCode() {
+        return eanCode;
+    }
+}
