476bb076e6479530497b23e6dd3c483c7d25fc05
johan <damstromjohan@gmail.com>
2022-10-10 14:32:12 +0200

    EAN class constructor updated

--- src/main/java/EAN.java
+++ src/main/java/EAN.java
@@ -1,7 +1,14 @@
+import java.util.IllegalFormatException;
+
 public class EAN {
     private final long eanCode;
 
     public EAN(long eanCode) {
+        if (String.valueOf(eanCode).length() < 12) {
+            throw new IllegalArgumentException("The EAN code is too short!");
+        } else if (String.valueOf(eanCode).length() > 13) {
+            throw new IllegalArgumentException("The EAN code is too long!");
+        }
         this.eanCode = eanCode;
     }
 
