641b3820f88b0404154be869e1cb062b97a21090
johan <damstromjohan@gmail.com>
2022-10-12 10:25:35 +0200

    checkout improvements

--- src/main/java/Checkout.java
+++ src/main/java/Checkout.java
@@ -1,2 +1,26 @@
+import java.util.ArrayList;
+
 public class Checkout {
+    int id;
+    int employeeId;
+    ArrayList<Integer> orderIds;
+    int balanceID;
+    public Checkout(int id, int employeeId, ArrayList<Integer> orderIds, int balanceId) {
+        this.id = id;
+        this.employeeId = employeeId;
+        this.orderIds = orderIds;
+        this.balanceID = balanceId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void changeEmployee(int employeeId) {
+        this.employeeId = employeeId;
+    }
 }
