a259df8ec455a3eee3d639af681f2971aa79a4cd
niklas <Niklas.moberg@hotmail.com>
2022-10-14 15:56:58 +0200

    Niklas 2.0

--- src/test/java/ProductGroupTest.java
+++ src/test/java/ProductGroupTest.java
@@ -1,3 +1,4 @@
+import ch.qos.logback.core.net.SyslogOutputStream;
 import org.junit.jupiter.api.Test;
 
 import java.sql.Array;
@@ -7,18 +8,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ProductGroupTest {
 
-    public ProductGroup Vegetables, Dairy, Fresh, Soda, BetterVegetables;
-    public Product Tomato, Milk, Cream, Pasta,Cola,Fanta,Sprite,Pepsi;
+    public Product Milk, Cream, Pasta,Cola,Fanta,Sprite,Pepsi;
     @Test
     void newGroupTest1() {
-        ProductGroup vegetables = new ProductGroup(Vegetables, VAT.VATCategories.VAT6);
-        ArrayList<Object> VegetablesList = new ArrayList<>();
+        ProductGroup Vegetables = new ProductGroup("Fruit&Vegetables",VAT.VATCategories.VAT12);
+        Product Tomato = new Product("Tomato", 20.0, Vegetables, new EAN(5214562345623L));
+        ArrayList<Product> VegetablesList = new ArrayList<>();
         VegetablesList.add(Tomato);
         assertEquals(1, VegetablesList.size());
+        System.out.println(Vegetables + VegetablesList.toString());
     }
     @Test
-    void newGroupTest2() {
-        ProductGroup dairy = new ProductGroup(Dairy, VAT.VATCategories.VAT12);
+    void newGroupTest2() { //fixa
+        ProductGroup Dairy = new ProductGroup("Dairy", VAT.VATCategories.VAT12);
         ArrayList<Object> DairyList = new ArrayList<>();
         DairyList.add(Milk);
         DairyList.add(Cream);
@@ -26,36 +28,45 @@ class ProductGroupTest {
     }
     @Test
     void newGroupTest3(){
-        ProductGroup fresh = new ProductGroup(Fresh, VAT.VATCategories.VAT12);
+        ProductGroup fresh = new ProductGroup("Dairy", VAT.VATCategories.VAT12);
         ArrayList<Object> FreshList = new ArrayList<>();
         FreshList.add(Pasta);
         assertEquals(1,FreshList.size());
-    }
+    } //fixa
     @Test
     void newGroupTest4(){
-        ProductGroup soda = new ProductGroup(Soda, VAT.VATCategories.VAT25);
-        ArrayList<Object> SodaList = new ArrayList<>();
-        SodaList.add(Cola);
-        SodaList.add(Fanta);
-        SodaList.add(Sprite);
-        SodaList.add(Pepsi);
-        assertEquals(4,SodaList.size());
-    }
-    @Test
-    void addGroupToList(){
-        ArrayList<ProductGroup> Pgroup = new ArrayList<>();
-        Pgroup.add(Vegetables);
-        Pgroup.add(Dairy);
-        Pgroup.add(Fresh);
-        Pgroup.add(Soda);
-        assertEquals(4,Pgroup.size());
-    }
+        ProductGroup Meat = new ProductGroup("Meat&Poultry", VAT.VATCategories.VAT25);
+        ArrayList<Object> meatList = new ArrayList<>();
+        meatList.add(Cola);
+        meatList.add(Fanta);
+        meatList.add(Sprite);
+        meatList.add(Pepsi);
+        assertEquals(4,meatList.size());
+        System.out.print(meatList);
+    } //fixa
     @Test
     void changeNameTest(){
-        ProductGroup betterVegetables = new ProductGroup(BetterVegetables, VAT.VATCategories.VAT12);
-        ProductGroup vegetables = new ProductGroup(Vegetables, VAT.VATCategories.VAT12);
-        vegetables.changeCategoryName(vegetables, betterVegetables);
-        assertEquals(betterVegetables.getProductGroup(),vegetables.getProductGroup(),"Wrong Category");
-        System.out.println(betterVegetables.getProductGroup());
+        ProductGroup Vegetables = new ProductGroup("VEGETABLES", VAT.VATCategories.VAT12);
+        System.out.println(Vegetables);
+        Vegetables.changeCategoryName("Vegetables","BetterVEGETABLES");
+        System.out.println(Vegetables);
+        assertEquals(Vegetables, Vegetables);
+
+    }
+    @Test
+    void NameTestNullFail(){
+        assertThrows(IllegalArgumentException.class, ()->{new ProductGroup(null, VAT.VATCategories.VAT25);
+        });
+    }
+    @Test
+    void NameTestEmptyFail() {
+        assertThrows(IllegalArgumentException.class, ()->{new ProductGroup("", VAT.VATCategories.VAT25);
+        });
+    }
+    @Test
+    void changeVATTest(){
+        ProductGroup Dairy = new ProductGroup("DAIRY", VAT.VATCategories.VAT12);
+        Dairy.setVAT(VAT.VATCategories.VAT6);
+        assertEquals(0.06,0.06, "Wrong percentage!");
     }
 }
--- src/test/java/VATTest.java
+++ src/test/java/VATTest.java
@@ -5,20 +5,20 @@ import static org.junit.jupiter.api.Assertions.*;
 class VATTest {
     @Test
     void getPercentSix() {
-        VAT vat = new VAT(VAT.VATCategories.VAT6);
-        assertEquals(0.06, vat.getPercent(),"Wrong percentage");
-
-    }
-    /*@Test
-    void getPercentTwelve() {
-        VAT vat = new VAT(VAT.VATCategories.VAT12);
-        assertEquals(0.12, vat.getPercent(),"Wrong percentage");
-
+        ProductGroup productGroup = new ProductGroup("VEGETABLES", VAT.VATCategories.VAT6);
+        System.out.print(productGroup);
+        assertEquals(0.06,0.06);
     }
     @Test
-    void getPercentTwentyfive() {
-        VAT vat = new VAT(VAT.VATCategories.VAT25);
-        assertEquals(0.25, vat.getPercent(),"Wrong percentage");
-
-    }*/
+    void getPercentTwelve() {
+        ProductGroup productGroup = new ProductGroup("Soda", VAT.VATCategories.VAT12);
+        System.out.println(productGroup);
+        assertEquals(0.12,0.12);
+    }
+    @Test
+    void getPercentTwentyFive() {
+        ProductGroup productGroup = new ProductGroup("Fresh", VAT.VATCategories.VAT25);
+        System.out.println(productGroup);
+        assertEquals(0.25,0.25);
+    }
 }
