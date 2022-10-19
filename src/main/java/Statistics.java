c735163c8875a766d2376e40dc666ee1b300ae7e
Theo <104140307+thwstn@users.noreply.github.com>
2022-10-19 23:48:47 +0200

    ny metod i statistics

--- src/main/java/Statistics.java
+++ src/main/java/Statistics.java
@@ -1,18 +1,19 @@
 
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.*;
 
 public class Statistics {
 
     FakeEmployeeDatabase fakeEmployeeDatabase;
     FakeOrderDatabase fakeOrderDatabase;
     FakeProductDatabase fakeProductDatabase;
+    public FakeCheckOutSessionDatabase fakeCheckOutSessionDatabase;
 
 
     public Statistics() {
         fakeEmployeeDatabase = new FakeEmployeeDatabase();
         fakeOrderDatabase = new FakeOrderDatabase();
         fakeProductDatabase = new FakeProductDatabase();
+        fakeCheckOutSessionDatabase = new FakeCheckOutSessionDatabase();
 
     }
 
@@ -25,6 +26,8 @@ public class Statistics {
         }
         return totalSalary / counter;
     }
+
+    //totalSalary
 
     public Product getCustomerMostSold(Customer customer) {
         Order fuling = new Order(new Employee("Theo", 12));
@@ -50,6 +53,62 @@ public class Statistics {
 
     //kundens mest köpta vara
 
+    //den som scannar flest produkter per arbetad timme
+
+    public LinkedHashMap<String, Double> getEmployeesSortedBySpeed(ArrayList<CheckOutSession> checkOutSessions) {
+        TreeMap<String, Integer> employeeNoOfProducts = new TreeMap<>();
+        for (Employee employee : fakeEmployeeDatabase.get()) {
+            int quantity = 0;
+            for (Order order : employee.getOrders()) {
+                for(OrderLine orderLine : order.getOrderLineList()) {
+                    quantity = quantity + orderLine.getQuantity();
+                }
+            }
+            employeeNoOfProducts.put(employee.getName(), quantity);
+        }
+//        TreeMap<String, Integer> employeeTotalSessionTime = new TreeMap<>();
+//        for (Employee employee : fakeEmployeeDatabase.get()) {
+//            int totalTime = 0;
+//            for (CheckOutSession checkOutSession : employee.getCheckOutSessions()) {
+//                totalTime = totalTime + checkOutSession.getSessionLenghtInSeconds();
+//            }
+//            employeeTotalSessionTime.put(employee.getName(), totalTime);
+//        }
+        TreeMap<String, Integer> employeeTotalSessionTime = new TreeMap<>();
+        for (CheckOutSession checkOutSession : checkOutSessions) {
+            String name = checkOutSession.getEmployee().getName();
+            int totalTime = checkOutSession.getSessionLenghtInSeconds();
+            employeeTotalSessionTime.put(name, totalTime);
+        }
 
 
+        TreeMap<String, Double> employeesWithSpeed = new TreeMap<>();
+        for (Employee employee : fakeEmployeeDatabase.get()) {
+            int products = employeeNoOfProducts.get(employee.getName());
+            int time = employeeTotalSessionTime.get(employee.getName());
+            double speed = (double) products / time;
+            employeesWithSpeed.put(employee.getName(), speed);
+        }
+        LinkedHashMap<String, Double> employeesWithSpeedSorted = new LinkedHashMap<>();
+        employeesWithSpeed.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue())
+                .forEachOrdered(x -> employeesWithSpeedSorted.put(x.getKey(), x.getValue()));
+        //Hämta alla employees
+        //För varje employee, hämta antal ordrar och antal sessions
+        //Räkna ut hur många varor de gör per session
+        //Returnera lista ordnad efter mest effektiv...
+        return employeesWithSpeedSorted;
+    }
+
+    public int getAverageCheckOutSessionLength(Employee employee, FakeCheckOutSessionDatabase fos) {
+        ArrayList<CheckOutSession> checkOutSessions = fos.getCheckOutSessionFromDatabaseWithEmployee(employee);
+        int totalSeconds = 0;
+        int counter = 0;
+        for (CheckOutSession checkOutSession : checkOutSessions) {
+            totalSeconds = totalSeconds + checkOutSession.getSessionLenghtInSeconds();
+            counter++;
+        }
+        return totalSeconds / counter;
+    }
 }
--- src/test/java/StatisticsTest.java
+++ src/test/java/StatisticsTest.java
@@ -5,6 +5,7 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
+import org.mockito.Mockito;
 
 public class StatisticsTest {
 
@@ -13,13 +14,47 @@ public class StatisticsTest {
     FakeOrderDatabase fakeOrderDatabase;
     FakeCustomerDatabase fakeCustomerDatabase;
     FakeProductDatabase fakeProductDatabase;
+    FakeCheckOutSessionDatabase fakeCheckOutSessionDatabase;
+    CheckOutSession checkOutSession1;
+    CheckOutSession checkOutSession2;
+    CheckOutSession checkOutSession3;
+    ArrayList<CheckOutSession> checkOutSessionsA;
+    Employee employee1;
+    Employee employee2;
+    Employee employee3;
 
     @BeforeEach
         void init() {
-            fakeEmployeeDatabase = new FakeEmployeeDatabase();
-            statistics = new Statistics();
-            fakeCustomerDatabase = new FakeCustomerDatabase();
-            fakeProductDatabase = new FakeProductDatabase();
+        fakeEmployeeDatabase = new FakeEmployeeDatabase();
+        statistics = new Statistics();
+        fakeCustomerDatabase = new FakeCustomerDatabase();
+        fakeProductDatabase = new FakeProductDatabase();
+        fakeCheckOutSessionDatabase = Mockito.mock(FakeCheckOutSessionDatabase.class);
+
+        checkOutSession1 = Mockito.mock(CheckOutSession.class);
+        checkOutSession2 = Mockito.mock(CheckOutSession.class);
+        checkOutSession3 = Mockito.mock(CheckOutSession.class);
+        employee1 = fakeEmployeeDatabase.getEmployee("Anna");
+        employee2 = fakeEmployeeDatabase.getEmployee("Calle");
+        employee3 = fakeEmployeeDatabase.getEmployee("Daniella");
+        Mockito.when(checkOutSession1.getSessionLenghtInSeconds()).thenReturn(28800);
+        Mockito.when(checkOutSession1.getEmployee()).thenReturn(employee1);
+        Mockito.when(checkOutSession2.getSessionLenghtInSeconds()).thenReturn(14400);
+        Mockito.when(checkOutSession2.getEmployee()).thenReturn(employee2);
+        Mockito.when(checkOutSession3.getSessionLenghtInSeconds()).thenReturn(7200);
+        Mockito.when(checkOutSession3.getEmployee()).thenReturn(employee3);
+
+        checkOutSessionsA = new ArrayList<>();
+        checkOutSessionsA.add(checkOutSession1);
+        checkOutSessionsA.add(checkOutSession2);
+        checkOutSessionsA.add(checkOutSession3);
+
+
+        //Mockito.when(fakeCheckOutSessionDatabase.getCheckOutSessionFromDatabaseWithEmployee(employee1)).thenReturn(checkOutSessionsA);
+        //Mockito.when(checkOutSessionsA.get(0)).thenReturn(checkOutSession1);
+        //Mockito.when(checkOutSessionsA.get(1)).thenReturn(checkOutSession2);
+        //Mockito.when(checkOutSessionsA.get(2)).thenReturn(checkOutSession3);
+        //Mockito.when(fakeCheckOutSessionDatabase.getCheckOutSessionFromDatabaseWithEmployee(fakeEmployeeDatabase.getEmployee("Anna"))).thenReturn(checkOutSessionsA);
     }
 
     @Test
@@ -33,5 +68,16 @@ public class StatisticsTest {
         System.out.println(pasta);
         Product p = statistics.getCustomerMostSold(fakeCustomerDatabase.getCustomer("Jacob"));
         assertEquals(pasta, p);
+    }
+
+    @Test
+    void getEmployeeAverageCheckOutSessionLengthTest() {
+        //ArrayList<CheckOutSession> ls = fakeCheckOutSessionDatabase.getCheckOutSessionFromDatabaseWithEmployee(fakeEmployeeDatabase.getEmployee("Anna"));
+        assertEquals(16800, statistics.getAverageCheckOutSessionLength(employee1, fakeCheckOutSessionDatabase));
+    }
+
+    @Test
+    void getEmployeesSortedBySpeedTest() {
+        System.out.println(statistics.getEmployeesSortedBySpeed(checkOutSessionsA));
     }
 }
