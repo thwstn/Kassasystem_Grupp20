1bc177e497505432873ea1499bc21744d9b7c1eb
Jacob <jacob_andersson@live.se>
2022-10-27 13:03:46 +0200

    Last Commit.

--- src/test/java/EANTest.java
+++ src/test/java/EANTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class EANTest {
@@ -9,5 +10,10 @@ public class EANTest {
         assertThrows(IllegalArgumentException.class, () -> {
             EAN ean = new EAN(123456789);
         });
+    }
+    @Test
+    void getEanTestReturnsCorrectValue(){
+        EAN ean = new EAN(123123123123L);
+        assertEquals(123123123123L, ean.getEANCode());
     }
 }
